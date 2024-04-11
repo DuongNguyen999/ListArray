64a2bba864451d9d928504bea828163d1695cad5
DuongNguyen999 <165481404+DuongNguyen999@users.noreply.github.com>
2024-04-11 22:19:27 +0900

    Add du an
    Add du an

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> mylists = new List<int>();
+        for (int i = 0; i < 10; i++)
+        {
+        mylists.Add(i);
+        }
+        Console.WriteLine(mylists.Count);
+        
+         mylists.RemoveAt(5);
+        foreach (var item in mylists)
+        {
+            Console.Write(item+",");
+        }
+       
+    }
+}
+//Count so phan tu bao gom trong list
+//Add them phan tu list
+//Removeat(indext);
+//Clear xoa toan bo
